6e52ae487a040e63412485a5f704640411afa2ec
loonarsliving <loonarsliving@gmail.com>
2026-06-04 23:39:55 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -327,7 +327,8 @@ function UploadSheet({onClose,branch,sub,toast}){
   const loginGoogle=()=>{
     const params=new URLSearchParams({
       client_id:GOOGLE_CLIENT_ID,
-      redirect_uri:https://filemanager.haluoleo.id
+    
+redirect_uri:"https://filemanager.haluoleo.id",
 ,
       response_type:"token",
       scope:DRIVE_SCOPE,
